bb52caeba393086dc3f8b213e1afe0289a221ae1
RenyAlexSilicus <reny.alex@silicus.com>
2018-06-06 11:49:02 +0530

    Second Commit.

--- app/src/main/java/com/silicus/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/silicus/kotlindemo/MainActivity.kt
@@ -2,13 +2,24 @@ package com.silicus.kotlindemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    //private var txtHelloWorld: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         println("Hello World")
+
+        val txtHelloWorld: TextView = findViewById(R.id.txtHelloWorld) as TextView
+        txtHelloWorld.setText("Hello World, This build is from VSAC ")
+
+        /*txtHelloWorld.setOnClickListener {
+            txtHelloWorld.text = "Hello World, This build is from VSAC "
+        }*/
+
     }
 }
